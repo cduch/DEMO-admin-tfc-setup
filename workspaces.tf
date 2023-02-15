d8018c330ca176111867433d4809254b925b7d00
Carsten Duch <carsten@hashicorp.com>
2023-02-15 14:02:42 +0100

    added project id

--- workspaces.tf
+++ workspaces.tf
@@ -13,7 +13,7 @@ resource "tfe_workspace" "hvnworkspace" {
   description        = "Workspace is managed via TFC."
   allow_destroy_plan = false
   auto_apply         = true
-  project_id         = tfe_project.networkproject
+  project_id         = tfe_project.networkproject.id
   vcs_repo {
     identifier = var.ghrepohvn
     oauth_token_id = var.githuboauthtokenid
