365aacc2b4c36c1c64c2857a70c6c68924ba49be
Carsten Duch <carsten@hashicorp.com>
2023-02-15 16:07:50 +0100

    added sec proj

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ resource "tfe_project" "adminproject" {
   name = "${var.prefix}-Admin"
 }
 
+resource "tfe_project" "securityproject" {
+  name = "${var.prefix}-Security"
+}
+
 resource "tfe_project" "networkproject" {
   name = "${var.prefix}-Network"
 }
